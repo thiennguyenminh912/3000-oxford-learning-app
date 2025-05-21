8baafa701e4f07172319e3971c268e4eb00c3716
Thiện Nguyễn <thiennguyen@Mac-mini-cua-Thien.local>
2025-05-22 00:21:53 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: "/3000-oxford-learning-app/",
 });
